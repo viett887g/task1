23e451b765b8a8c7c8815043b8a4618c06484f11
viett887g <91525508+viett887g@users.noreply.github.com>
2023-03-06 21:15:56 +0700

    Add files via upload

---
+++ bai2.java
@@ -0,0 +1,42 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package javaapplication2;
+
+
+import java.lang.Math;
+import java.util.Scanner;
+
+/**
+ *
+ * @author ADMIN
+ */
+
+public class bai2 {
+    public static void main(String[] args){
+        boolean flag =true;
+        int a=11;
+        byte b=2;
+        float c = (float) 1.26;
+        float d= 11.4f;
+        String s = "Hello";
+        char v = 'n';
+        Scanner na = new Scanner(System.in);
+        System.out.println("hom nay ban cam thay the nao");
+        String fell = na.nextLine();
+        System.out.println("cam giac la : "+ fell);
+        
+        System.out.println("nhap vao 1 so gnuyen");
+        String input = na.nextLine();
+        a= Integer.parseInt(input);
+        System.out.println("so nguyen la : "+ a);
+        
+        
+        
+        
+        
+    }
+    
+    
+}
---
+++ bai3.java
@@ -0,0 +1,50 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package javaapplication2;
+
+/**
+ *
+ * @author ADMIN
+ */
+public class bai3 {
+    public static void main(String[] args) {
+        int a=5,b=7,c,d,e,f;
+        c=a-b;
+        d=a+b;
+        e=a*b;
+        f=a/b;
+        System.out.println("phep cong cua a va b la "+d);
+        System.out.println("phep tru cua a va b la "+c);
+        System.out.println("phep nhan cua a va b la "+e);
+        System.out.println("phep chia cua a va b la "+f);
+        
+       
+        double a1=5.6 , b1=7.3,c1,d1,e1,f1;
+        c1=a1-b1;
+        d1=a1+b1;
+        e1=a1*b1;
+        f1=a1/b1;
+        System.out.println("phep cong cua a va b la "+d1);
+        System.out.println("phep tru cua a va b la "+c1);
+        System.out.println("phep nhan cua a va b la "+e1);
+        System.out.println("phep chia cua a va b la "+f1);
+        if (b+a>4 && a>c){
+            System.out.println("phep and");
+        if (a>5 || b>5){
+            System.out.println("phep or");
+        
+        String s1 ="Haafnfioifief";
+        String s2 ="anffkandadwddwj";
+        System.out.println(s1.toLowerCase());
+        System.out.println(s1.toUpperCase());
+        System.out.println(s1.contains(s2));
+        System.out.println(s1+"  "+s2);
+        }
+    }
+  
+            
+    }
+    
+}
